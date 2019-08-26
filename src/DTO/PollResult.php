fe3d4f561d1dde5b95252ea671657ec33284e8ca
Anthony Poon <ypoon196@gmail.com>
2019-08-26 16:49:08 +0800

    c1k first commit

---
+++ src/DTO/PollResult.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\DTO;
+
+
+use JsonSerializable;
+
+class PollResult implements JsonSerializable {
+    const LABELS = [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5"
+    ];
+    private $results = [];
+    public function __construct() {
+        foreach (self::LABELS as $label) {
+            $this->results[$label] = 0;
+        }
+    }
+
+    public function setResultByLabel(String $label, $count) {
+        $this->results[$label] = $count;
+    }
+
+    public function getResultByLabel(String $label) {
+        return $this->results[$label];
+    }
+
+    public function jsonSerialize() {
+        $rtn = [];
+        foreach ($this->results as $label => $count) {
+            $rtn[] = [
+                "label" => (int) $label,
+                "count" => (int) $count
+            ];
+        }
+        return $rtn;
+    }
+
+
+}
